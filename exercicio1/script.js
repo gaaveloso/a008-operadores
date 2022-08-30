75c22dee258e551f99dad2d769b220541dfc3782
gaaveloso <gabrielvelosorezende-po@hotmail.com>
2022-08-30 12:11:13 -0300

    exercicios da aula 008 resolvido

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let numero = Number(prompt("Insira um número PAR:"))
+console.log("O resto da divisão de", numero, "por 2 é igual a :", numero%2)
+console.log(typeof numero)
+
+// Todos os restos das divisões de números pares por 2 é igual a 0.
+// Se o usuario inserir um número impar, o resto da divisão por 2 vai ser igual a 1.
